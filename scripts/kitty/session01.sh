8e645ccc688922b075f014ff761fe1e1e7d1eaef
xezwary <xezwary@proton.me>
2023-12-12 09:08:32 +0100

    Update session script

--- scripts/kitty/session01.sh
+++ scripts/kitty/session01.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-kitty @ launch --type tab --tab-title "newsboat" newsboat
+kitty @ launch --type tab --tab-title "newsboat" filledprompt "newsboat"
 kitty @ launch --type tab --cwd ~/Music/playlists --tab-title "music" filledprompt "~/scripts/bash/commands/mpv_music.sh -s 1 -v 70 -e 2 -i TAM.m3u"
 kitty @ launch --type tab --tab-title "vimwiki" vim -c "VimwikiIndex"
 kitty @ launch --type tab
