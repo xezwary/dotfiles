0b34f3e4d7d6ca9dd2577fbf2bda4dc6985728fa
xezwary <onemnstr@tuta.io>
2024-03-07 03:02:24 +0100

    Notify when mpv fails to load video

--- scripts/bash/commands/mpv_youtubedl_quality.sh
+++ scripts/bash/commands/mpv_youtubedl_quality.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-[[ $1 ]]&&url=$(echo "$1" | sed -e "s|www\.youtube\.com|yewtu\.be|")&&setsid -f mpv --no-terminal --force-window --quiet -- $url 2>/dev/null >/dev/null 2>&1
+[[ $1 ]]&&(mpv --no-terminal --force-window --quiet -- $(echo "$1" | sed -e "s|www\.youtube\.com|invidious\.private\.coffee|" -e "s|youtube\.com|invidious\.private\.coffee|") 2>/dev/null >/dev/null 2>&1||notify-send "MPV failed.")
