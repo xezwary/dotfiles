2c8f2f238d1ad5b7c3c197023cb84f22ae4e1526
xezwary <onemnstr@tuta.io>
2024-06-16 00:27:02 +0200

    Some fixes

--- scripts/bash/commands/mpv_youtubedl_quality.sh
+++ scripts/bash/commands/mpv_youtubedl_quality.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-[[ $1 ]]&&(mpv --no-terminal --force-window --quiet -- $(echo "$1" | sed -e "s|www\.youtube\.com|invidious\.private\.coffee|" -e "s|youtube\.com|invidious\.private\.coffee|") 2>/dev/null >/dev/null 2>&1||notify-send "MPV failed.")
+[[ $1 ]]&&(mpv --no-terminal --force-window --quiet -- "$1" 2>/dev/null >/dev/null 2>&1||notify-send "MPV failed.")
