83ffc0ac814676b37e939818391c42457fbe0e74
xezwary <onemnstr@tuta.io>
2024-06-16 00:31:50 +0200

    More fixes

--- scripts/bash/commands/mpv_youtubedl_quality.sh
+++ scripts/bash/commands/mpv_youtubedl_quality.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-[[ $1 ]]&&(mpv --no-terminal --force-window --quiet -- "$1" 2>/dev/null >/dev/null 2>&1||notify-send "MPV failed.")
+[[ $1 ]]&&(mpv --no-terminal --force-window --quiet -- "$1" >/dev/null 2>&1||notify-send "MPV failed.")
